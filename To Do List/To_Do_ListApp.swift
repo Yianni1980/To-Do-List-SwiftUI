274483bb2308e5bc1a3d5ac533fee33f6cd4c2e7
yianni1980 <ioagian1980@gmail.com>
2023-05-01 19:41:01 +0300

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by ioannis on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
