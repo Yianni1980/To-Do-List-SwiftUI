2e6cc4a638180d9526c1aabfa819f49a36c4ba9e
yianni1980 <ioagian1980@gmail.com>
2023-05-09 20:43:16 +0300

    Sending items to firebase from NewItemView

--- To Do List/ViewModels/NewItemViewViewModel.swift
+++ To Do List/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel:ObservableObject {
     @Published var title = ""
@@ -14,9 +16,28 @@ class NewItemViewViewModel:ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
-        
-        
+        //get current userID
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        // create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        //Save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document("123")
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
